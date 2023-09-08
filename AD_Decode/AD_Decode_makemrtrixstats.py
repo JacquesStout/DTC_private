f20a2e84abf41d00c547345939df31103bcb9bee
Jacques Stout <jas@santorini.dhe.duke.edu>
2023-09-08 16:06:29 -0400

    adding the creation of mrtrix stats for AD_Decode with wrapper

--- AD_Decode/AD_Decode_makemrtrixstats.py
+++ AD_Decode/AD_Decode_makemrtrixstats.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os, shutil
+import os, shutil, re, fnmatch, glob
 
 def generate_bvec_gradscheme(bvecs_orig, bvals_orig, bvecs_new, bvals_new, gradscheme_path):
 
@@ -16,20 +16,28 @@ def generate_bvec_gradscheme(bvecs_orig, bvals_orig, bvecs_new, bvals_new, grads
     bvals_orig = np.round(bvals_orig, decimals=2)
     bvals_new = np.round(bvals_new, decimals=2)
 
+    bvecs_transpose = np.zeros(3)
     if np.any(bvecs_orig!=bvecs_new):
         num_bvecs = np.shape(bvecs_orig)[1]
         for i in range(3):
-            if all(bvecs_orig[i][j] == -bvecs_new[(i + 1) % 3][j] for j in range(num_bvecs)):
-                bvecs_transpose.append(f'-{chr(97 + i)}')
-            elif all(bvecs_orig[i][j] == bvecs_new[(i + 1) % 3][j] for j in range(num_bvecs)):
-                bvecs_transpose.append(f'{chr(97 + i)}')
+            #if all(bvecs_orig[i][j] == -bvecs_new[(i + 1) % 3][j] for j in range(num_bvecs)):
+            #    bvecs_transpose.append(f'-{chr(97 + i)}')
+            #elif all(bvecs_orig[i][j] == bvecs_new[(i + 1) % 3][j] for j in range(num_bvecs)):
+            #    bvecs_transpose.append(f'{chr(97 + i)}')
+            for j in range(3):
+                if np.all(np.abs(bvecs_orig[:][i] - bvecs_new[:][j]) < 2e-2):
+                    bvecs_transpose[i] = j + 1
+                if np.all(np.abs(bvecs_orig[:][i] + bvecs_new[:][j]) < 2e-2):
+                    bvecs_transpose[i] = -(j + 1)
     else:
-        bvecs_transpose = ['1, 2, 3']
+        bvecs_transpose = [1, 2, 3]
+    bvecs_transpose = list(bvecs_transpose)
     if np.any(bvals_orig!=bvals_new):
-        print('must investigate')
-        raise Exception
-    gradscheme_txt = f'bvecs: {bvecs_transpose}'
-
+        bvals_transpose = bvals_new/bvals_orig
+        bvals_transpose = [1 if np.isnan(x) else x for x in bvals_transpose]
+    else:
+        bvals_transpose = list(np.ones(np.size(bvals_orig)))
+    gradscheme_txt = f'bvecs: {bvecs_transpose}\n' + f'bvals: {bvals_transpose}'
     with open(gradscheme_path, 'w') as file:
         file.write(gradscheme_txt)
 
@@ -41,9 +49,11 @@ def save_new_bvecs(bvecs_orig, gradscheme_path, bvecs_new_path):
     #bvecs_new = [row[:] for row in bvecs_orig]  # Create a copy of the original matrix
 
     with open(gradscheme_path, 'r') as file:
-        bvecs_transpose = file.read()
-        bvecs_transpose = bvecs_transpose.split("'")[1].split(',')
-        bvecs_transpose = np.array(list(map(int, bvecs_transpose)))
+        for line in file:
+            if line.startswith('bvecs'):
+                bvecs_transpose = line.split("[")[1].split(']')[0].split(',')
+                #bvecs_transpose = np.array(list(map(int, bvecs_transpose)))
+                bvecs_transpose = [int(float(x)) for x in bvecs_transpose]
     #print(bvecs_transpose)
 
     bvecs_new = np.vstack((bvecs_orig[abs(bvecs_transpose[0])-1]*np.sign(bvecs_transpose[0]),
@@ -51,6 +61,79 @@ def save_new_bvecs(bvecs_orig, gradscheme_path, bvecs_new_path):
                            bvecs_orig[abs(bvecs_transpose[2])-1]*np.sign(bvecs_transpose[2])))
 
     np.savetxt(bvecs_new_path, bvecs_new, fmt='%.2f')
+
+
+def save_new_bvals(bvals_orig, gradscheme_path,bvals_newpath):
+    if isinstance(bvals_orig, str):
+        bvecs_orig=np.loadtxt(bvals_orig)
+    #bvecs_new = [row[:] for row in bvecs_orig]  # Create a copy of the original matrix
+
+    with open(gradscheme_path, 'r') as file:
+        for line in file:
+            if line.startswith('bvals'):
+                bvals_transpose = line.split("[")[1].split(']')[0].split(',')
+                bvals_transpose = [(float(x)) for x in bvals_transpose]
+
+    bvals_new = bvals_orig * list(bvals_transpose)
+    np.savetxt(bvals_newpath, bvals_new, fmt='%.2f')
+
+
+def regexify(string):
+    newstring = ('^'+string+'$').replace('*','.*')
+    return newstring
+
+
+def glob_remote(path, sftp=None):
+    match_files = []
+    if sftp is not None:
+        if '*' in path:
+            pathdir, pathname = os.path.split(path)
+            pathname = regexify(pathname)
+            allfiles = sftp.listdir(pathdir)
+            for file in allfiles:
+                if re.search(pathname, file) is not None:
+                    match_files.append(os.path.join(pathdir,file))
+        elif '.' not in path:
+            allfiles = sftp.listdir(path)
+            for filepath in allfiles:
+                match_files.append(os.path.join(path, filepath))
+            return match_files
+        else:
+            dirpath = os.path.dirname(path)
+            try:
+                sftp.stat(dirpath)
+            except:
+                return match_files
+            allfiles = sftp.listdir(dirpath)
+            #if '*' in path:
+            #    for filepath in allfiles:
+            #            match_files.append(os.path.join(dirpath,filepath))
+            #else:
+            for filepath in allfiles:
+                if fnmatch.fnmatch(os.path.basename(filepath), os.path.basename(path)):
+                    match_files.append(os.path.join(dirpath, filepath))
+    else:
+        if '.' not in path:
+            match_files = glob.glob(path)
+        else:
+            dirpath = os.path.dirname(path)
+            if not os.path.exists(dirpath):
+                return(match_files)
+            else:
+                allfiles = glob.glob(os.path.join(dirpath,'*'))
+                for filepath in allfiles:
+                    if fnmatch.fnmatch(os.path.basename(filepath), os.path.basename(path)):
+                        match_files.append(os.path.join(dirpath, filepath))
+    return(match_files)
+
+
+def checkallfiles(paths, sftp=None):
+    existing = True
+    for path in paths:
+        match_files = glob_remote(path, sftp)
+        if not match_files:
+            existing= False
+    return existing
 
 
 munin=False
@@ -71,11 +154,12 @@ else:
 
 
 subjects = ['00775', '04491', '04493', '01412', '04526', '01470']
-subjects = ['00775']
 proc_subjn="S"
 proc_name ="diffusion_prep_"+proc_subjn
 
 overwrite=False
+
+cleanup = True
 
 for subject in subjects:
     subj_path = os.path.join(outpath, proc_name+subject)
@@ -106,38 +190,63 @@ for subject in subjects:
     rd_nii = os.path.join(subj_path, subj + '_mrtrixrd.nii.gz')
     md_nii = os.path.join(subj_path, subj + '_mrtrixmd.nii.gz')
 
+    exists_all_nii = checkallfiles([dt_mif, fa_nii, ad_nii, rd_nii, md_nii])
 
-    if not os.path.exists(mask_mif) or overwrite:
-        command = f'mrconvert {mask_nii} {mask_mif}'
-        os.system(command)
+    if not exists_all_nii:
+        if not os.path.exists(mask_mif) or overwrite:
+            command = f'mrconvert {mask_nii} {mask_mif}'
+            os.system(command)
 
-    if not os.path.exists(bvecs_check) or overwrite:
-        os.system(
-            f'dwigradcheck ' + coreg_preproced + ' -fslgrad ' + bvecs_fixed + ' ' + bvals_fixed + ' -mask ' + mask_mif + ' -number 100000 -export_grad_fsl ' + bvecs_check + ' ' + bvals_check + ' -force')
 
-        generate_bvec_gradscheme(bvecs_fixed, bvals_fixed, bvecs_check, bvals_check, bvecs_grad_scheme_txt)
-    else:
-        bvec_orig = np.loadtxt(bvecs_fixed)
-        bval_orig = np.loadtxt(bvals_fixed)
+        if not os.path.exists(bvecs_fixed) or overwrite:
+            if not os.path.exists(bvecs_grad_scheme_txt):
+                os.system(
+                    f'dwigradcheck ' + coreg_preproced + ' -fslgrad ' + bvecs_fixed + ' ' + bvals_fixed + ' -mask ' + mask_mif + ' -number 100000 -export_grad_fsl ' + bvecs_check + ' ' + bvals_check + ' -force')
+                generate_bvec_gradscheme(bvecs_fixed, bvals_fixed, bvecs_check, bvals_check, bvecs_grad_scheme_txt)
+            else:
+                bvec_orig = np.loadtxt(bvecs_fixed)
+                bval_orig = np.loadtxt(bvals_fixed)
 
-        save_new_bvecs(bvec_orig, bvecs_grad_scheme_txt, bvecs_check)
-        shutil.copy(bvals_fixed, bvals_check)
+                save_new_bvecs(bvec_orig, bvecs_grad_scheme_txt, bvecs_check)
+                save_new_bvals(bval_orig, bvecs_grad_scheme_txt, bvals_check)
 
-    bvals = np.loadtxt(bvals_fixed)
-    if np.unique(bvals_fixed).shape[0] > 2 :
-        os.system \
-            ('dwi2tensor ' + coreg_preproced + ' ' + dt_mif + ' -dkt ' +  dk_mif +' -fslgrad ' + bvecs_check + ' ' + bvals_check + ' -force')
-        os.system(
-            'tensor2metric  -fa ' + fa_mif + ' ' + dt_mif + ' -adc ' + md_mif + ' -ad ' + ad_mif + ' -rd ' + rd_mif + ' -force')
+        bvals = np.loadtxt(bvals_fixed)
+        exists_all_mif = checkallfiles([dt_mif, fa_mif, rd_mif, ad_mif, md_mif])
 
-        # os.system('mrview '+ fa_mif) #inspect residual
-    else:
-        os.system(
-            'dwi2tensor ' + coreg_preproced + ' ' + dt_mif + ' -fslgrad ' + bvecs_check + ' ' + bvals_check + ' -force')
-        os.system('tensor2metric  -fa ' + fa_mif + ' ' + dt_mif + ' -force')
-        os.system('tensor2metric  -rd ' + rd_mif + ' ' + dt_mif + ' -force')  # if doesn't work take this out :(
-        os.system('tensor2metric  -ad ' + ad_mif + ' ' + dt_mif + ' -force')  # if doesn't work take this out :(
-        os.system('tensor2metric  -adc ' + md_mif + ' ' + dt_mif + ' -force')
+        if not exists_all_mif:
+            if np.unique(bvals_fixed).shape[0] > 2 :
+                os.system \
+                    ('dwi2tensor ' + coreg_preproced + ' ' + dt_mif + ' -dkt ' +  dk_mif +' -fslgrad ' + bvecs_check + ' ' + bvals_check + ' -force')
+                os.system(
+                    'tensor2metric  -fa ' + fa_mif + ' ' + dt_mif + ' -adc ' + md_mif + ' -ad ' + ad_mif + ' -rd ' + rd_mif + ' -force')
 
-    if not os.path.exists(fa_nii):
-        command = f'mrconvert {fa_mif} {fa_nii}'
+                # os.system('mrview '+ fa_mif) #inspect residual
+            else:
+                os.system(
+                    'dwi2tensor ' + coreg_preproced + ' ' + dt_mif + ' -fslgrad ' + bvecs_check + ' ' + bvals_check + ' -force')
+                os.system('tensor2metric  -fa ' + fa_mif + ' ' + dt_mif + ' -force')
+                os.system('tensor2metric  -rd ' + rd_mif + ' ' + dt_mif + ' -force')  # if doesn't work take this out :(
+                os.system('tensor2metric  -ad ' + ad_mif + ' ' + dt_mif + ' -force')  # if doesn't work take this out :(
+                os.system('tensor2metric  -adc ' + md_mif + ' ' + dt_mif + ' -force')
+
+        if not os.path.exists(fa_nii):
+            command = f'mrconvert {fa_mif} {fa_nii}'
+            os.system(command)
+
+        if not os.path.exists(rd_nii):
+            command = f'mrconvert {rd_mif} {rd_nii}'
+            os.system(command)
+
+        if not os.path.exists(ad_nii):
+            command = f'mrconvert {ad_mif} {ad_nii}'
+            os.system(command)
+
+        if not os.path.exists(md_nii):
+            command = f'mrconvert {md_mif} {md_nii}'
+            os.system(command)
+
+        if cleanup:
+            os.remove(fa_mif)
+            os.remove(rd_mif)
+            os.remove(ad_mif)
+            os.remove(md_mif)
