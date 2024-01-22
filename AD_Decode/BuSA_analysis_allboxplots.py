4a28c5e157bdbc184a731dc277788a3927e6bc3e
Jacques Stout <jas@santorini.local>
2024-01-22 14:38:31 -0500

    major update to allow side or no side flagging, showing APOE3vAPOE4 boxplots

--- AD_Decode/BuSA_analysis_allboxplots.py
+++ AD_Decode/BuSA_analysis_allboxplots.py
@@ -17,7 +17,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import socket
 from DTC.file_manager.file_tools import mkcdir, check_files
-
+from DTC.file_manager.computer_nav import checkfile_exists_remote, get_mainpaths, load_nifti_remote, load_trk_remote, \
+    loadmat_remote, pickledump_remote, remote_pickle, checkfile_exists_all, write_parameters_to_ini, \
+    read_parameters_from_ini
 
 def outlier_removal(values, qsep=3):
     iqr = abs(np.quantile(values, 0.25) - np.quantile(values, 0.75))
@@ -29,7 +31,8 @@ def outlier_removal(values, qsep=3):
 remote = False
 
 if len(sys.argv) < 2:
-    project = 'V0_9_10template_100_6_interhe_majority'
+    #project = 'V0_9_10template_100_6_interhe_majority'
+    project = 'V0_9_reg_superiorfrontalleft_precentralleft_6'
 else:
     project = sys.argv[1]
 
@@ -58,9 +61,30 @@ if remote:
 else:
     sftp = None
 
+project_headfile_folder = '/Volumes/Data/Badea/Lab/jacques/BuSA_headfiles/'
+project_summary_file = os.path.join(project_headfile_folder,project+'.ini')
+
+if not os.path.exists(project_summary_file):
+    txt = f'Could not find configuration file at {project_summary_file}'
+    raise Exception(txt)
+else:
+    params = read_parameters_from_ini(project_summary_file)
+
+
+template_subjects = params['template_subjects']
+added_subjects = params['added_subjects']
+num_bundles = int(params['num_bundles'])
+removed_list = params['removed_list']
+
+full_subjects_list = template_subjects + added_subjects
+
+for remove in removed_list:
+    if remove in full_subjects_list:
+        full_subjects_list.remove(remove)
+
 master_df = pd.read_csv(master)
 
-length_cut = 40
+length_cut = None
 if length_cut is None:
     length_str = ''
 else:
@@ -72,24 +96,12 @@ stats_path = os.path.join(root, 'stats')
 
 mkcdir([stats_path, figures_path, excel_path])
 
-if remote:
-    all_subj_bundles = glob_remote(stats_path, sftp)
-else:
-    all_subj_bundles = os.listdir(stats_path)
+siding = False
 
-ref_subj = 'S02224'
-bundles = [i for i in all_subj_bundles if ref_subj in i]
-bundles = [i[6:] for i in bundles]
-bundles = sorted(bundles)
-
-bundles_new = []
-for bundle in bundles:
-    if 'comparison' not in bundle:
-        bundles_new.append(bundle)
-bundles = bundles_new
+if not siding:
+    bundles = [f'_bundle_{bundle_id}.xlsx' for bundle_id in np.arange(num_bundles)]
 
 num_groups = 5
-num_bundles = 6
 
 tr_list = [0] + [np.quantile(master_df['age'], (i + 1) * (1 / num_groups)) for i in np.arange(num_groups)] + [
     1 + np.max(master_df['age'])]
@@ -99,10 +111,20 @@ tr_list = [0] + [np.quantile(master_df['age'], (i + 1) * (1 / num_groups)) for i
 
 test = False
 
+if not siding:
+    side_spec = ''
+    sides = ['all']
+else:
+    side_spec = 'Side_'
+
 if test:
     bundles = [f'_left_bundle_0.xlsx']
 
-columns_list = [f'Side_{bundle.split("_")[1]}_ID_{bundle.split("_")[3].split(".")[0]}' for bundle in bundles]
+if not siding:
+    columns_list = [f'ID_{bundle.split("_")[2].split(".")[0]}' for bundle in bundles]
+else:
+    columns_list = [f'{side_spec}{bundle.split("_")[1]}_ID_{bundle.split("_")[3].split(".")[0]}' for bundle in bundles]
+
 datameans = pd.DataFrame(columns=(['Age Group'] + columns_list))
 datasds = pd.DataFrame(columns=(['Age Group'] + columns_list))
 
@@ -119,34 +141,50 @@ mkcdir([figures_box_path, figures_agegrouping_path])
 # sds = {}
 
 testmode = False
+verbose = True
+geno_simplify = True
 
 for bundle in bundles:
 
-    if 'left' in bundle:
-        side = 'left'
-    if 'right' in bundle:
-        side = 'right'
+    print(f'Beginning for bundle {bundle}')
+
+    if not siding:
+        side = ''
+        side_str = ''
+    else:
+        if 'left' in bundle:
+            side = 'left'
+        if 'right' in bundle:
+            side = 'right'
+        side_str = '_' + side
+
     bundle_num = bundle.split('bundle_')[1].split('.')[0]
 
-    fig_bundle_path = os.path.join(figures_box_path, f'bundle_{side}_{bundle_num}_boxsquaremodel.png')
+    fig_bundle_path = os.path.join(figures_box_path, f'bundle{side_str}_{bundle_num}_boxsquaremodel.png')
+    """
     this_bundle_subjs = [i for i in all_subj_bundles if bundle in i]
     this_bundle_subjs = sorted(this_bundle_subjs)
     if testmode:
         this_bundle_subjs = this_bundle_subjs[:3]
-
+    """
     # bundle_df = pd.DataFrame()
-    for subj in this_bundle_subjs:
+    for subj in full_subjects_list:
 
-        if remote:
-            load_df_remote(os.path.join(stats_path, subj), sftp)
-        else:
-            temp = pd.read_excel(os.path.join(stats_path, subj))
+        subj_bundle_path = os.path.join(stats_path, subj+bundle)
+        try:
+            if remote:
+                load_df_remote(subj_bundle_path, sftp)
+            else:
+                temp = pd.read_excel(subj_bundle_path)
+        except:
+            print(f'Not including subject {subj}')
 
         # temp = pd.DataFrame()
         temp['Subject'] = subj[2:6]
         index = master_df["MRI_Exam"] == int(subj[2:6])
         try:
             temp['age'] = master_df[index]['age'].iloc[0]
+            temp['genotype'] = master_df[index]['genotype'].iloc[0]
             # temp['sex'] = master_df[index]['sex']
         except:
             print(f'Subject {subj[:6]} is missing from excel database')
@@ -167,15 +205,21 @@ for bundle in bundles:
         column_names.append("point_" + str(i) + f"_{ref}")
     bundle_df[f'average{ref}'] = np.mean(bundle_df[column_names], 1)
 
-    bundle_df_reduced = bundle_df[[f"average{ref}", "age"]]
+    bundle_df_reduced = bundle_df[[f"average{ref}", "age","genotype"]]
     #    bundle_df_reduced = bundle_df_reduced.iloc[ 0:2000]
 
     # bundle_df_reduced.boxplot(column='averageFA',by='age')
     # sns.boxplot(x="age", y="averageFA", data=bundle_df_reduced)
-    sns.lmplot(x="age", y=f"average{ref}", data=bundle_df_reduced, x_estimator=np.mean, order=2)
+    if geno_simplify:
+        bundle_df_reduced = bundle_df_reduced.replace(
+            {'APOE23': 'APOE3', 'APOE34': 'APOE4', 'APOE33': 'APOE3','APOE44':'APOE4'}, regex=True)
+
+    sns.lmplot(x="age", y=f"average{ref}", data=bundle_df_reduced, x_estimator=np.mean, order=2, hue='genotype')
     plt.title(f'Boxplot for bundle {int(bundle_num) + 1}', y=0.9)
 
     plt.savefig(fig_bundle_path)
+    if verbose:
+        print(f'Saved figure of bundles at {fig_bundle_path}')
 
     list_df = []
     for i in np.arange(num_groups):
@@ -191,9 +235,9 @@ for bundle in bundles:
         # means[int(i),int(bundle_num)] = np.mean(group)
         # sds[int(i),int(bundle_num)] = np.std(group)
         datameans.loc[i, 'Age Group'] = f'Age Group {i}'
-        datameans.loc[i, f'Side_{side}_ID_{bundle_num}'] = np.mean(group)
+        datameans.loc[i, f'{side_spec}{side_str}ID_{bundle_num}'] = np.mean(group)
         datasds.loc[i, 'Age Group'] = f'Age Group {i}'
-        datasds.loc[i, f'Side_{side}_ID_{bundle_num}'] = np.std(group)
+        datasds.loc[i, f'{side_spec}{side}_ID_{bundle_num}'] = np.std(group)
 
     del (bundle_df)
 
@@ -228,6 +272,8 @@ for i, bundle_name in enumerate(columns_list):
     plt.ylabel('Change in Values')
     plt.legend()
     plt.savefig(fig_plot_path)
+    if verbose:
+        print(f'Saved figure at {fig_plot_path}')
     plt.close()
 
 for i, bundle_name in enumerate(columns_list):
@@ -240,4 +286,6 @@ for i, bundle_name in enumerate(columns_list):
     plt.ylabel('Change in Values')
     plt.legend()
     plt.savefig(fig_plot_path)
+    if verbose:
+        print(f'Saved figure at {fig_plot_path}')
     plt.close()
