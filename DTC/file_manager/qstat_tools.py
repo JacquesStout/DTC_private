0362a9360b1db80b639562913644b9bd45e2ea92
jas297 <jacques.stout@duke.edu>
2024-02-13 15:43:07 -0500

    now can get full name of jobs

--- DTC/file_manager/qstat_tools.py
+++ DTC/file_manager/qstat_tools.py
@@ -3,18 +3,55 @@ from DTC.file_manager.file_tools import mkcdir, getfromfile
 from DTC.file_manager.computer_nav import read_parameters_from_ini
 from DTC.file_manager.BIAC_tools import send_mail
 import numpy as np
-import socket, glob, datetime
+import socket, glob, datetime, sys, string
+import xml.dom.minidom
 
 
 def get_running_jobs():
+
+    f=os.popen('qstat -u \* -xml -r')
+
+    dom=xml.dom.minidom.parse(f)
+
+    jobs=dom.getElementsByTagName('job_info')
+    run=jobs[0]
+
+    joblist=run.getElementsByTagName('job_list')
+
+    jobnames = []
+    for r in joblist:
+        try:
+            jobname=r.getElementsByTagName('JB_name')[0].childNodes[0].data
+            jobown=r.getElementsByTagName('JB_owner')[0].childNodes[0].data
+            jobstate=r.getElementsByTagName('state')[0].childNodes[0].data
+            jobnum=r.getElementsByTagName('JB_job_number')[0].childNodes[0].data
+            jobtime='not set'
+            if(jobstate=='r'):
+                jobtime=r.getElementsByTagName('JAT_start_time')[0].childNodes[0].data
+            elif(jobstate=='dt'):
+                jobtime=r.getElementsByTagName('JAT_start_time')[0].childNodes[0].data
+            else:
+                jobtime=r.getElementsByTagName('JB_submission_time')[0].childNodes[0].data
+
+            #print(jobnum, '\t', jobown.ljust(16), '\t', jobname.ljust(16),'\t', jobstate,'\t',jobtime)
+            jobnames.append(jobname.ljust(16).replace(' ',''))
+        except Exception as e:
+            print(e)
+    return(jobnames)
+
+"""
+def get_running_jobs():
     try:
-        output = subprocess.check_output(["qstat"]).decode("utf-8")
+        #output = subprocess.check_output(["qstat"]).decode("utf-8")
+        cmd = "qstat -xml | tr '\n' ' ' | sed 's#<job_list[^>]*>#\n#g'   | sed 's#<[^>]*>##g' | grep " " | column -t"
+        cmd_list = cmd.split(' ')
+        output = subprocess.check_output(cmd_list).decode("utf-8")
         lines = output.split('\n')[2:]  # Skip the header lines
         running_jobs = [line.split()[2] for line in lines if line.strip()]  # Extract job names
         return running_jobs
     except subprocess.CalledProcessError:
         return []
-
+"""
 
 def pause_jobs(verbose=False, email_sending = False):
     while True:
@@ -60,6 +97,8 @@ def limit_jobs(limit = 10, verbose=False):
 def check_job_name(jobname):
     running_jobs = get_running_jobs()
     non_interact_jobs = [job for job in running_jobs if job != 'interact']
+    print(non_interact_jobs)
+    print(jobname)
     if jobname in non_interact_jobs:
         return True
     else:
