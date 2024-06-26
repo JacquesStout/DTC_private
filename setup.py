2db0b4c2ef6b78a58bb6f0e27bb3331f63f592a7
Jacques Stout <jas@santorini.local>
2024-06-26 14:27:28 -0400

    updating the setup file for installing DTC with updated used packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.6.0'
+VERSION = '0.9.0'
 DESCRIPTION = 'Diffusion-images To Connectome processing toolbox'
 LONG_DESCRIPTION = 'Set of tools for processing of diffusion images into connectome form using dipy methods and others'
 
@@ -23,11 +23,11 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
-    install_requires=['antspyx==0.3.7','begins==0.9', 'Cython==0.29.28', 'dcor==0.5.3', 'dill==0.3.4', 'dipy==1.4.1',
-                      'fury==0.2.0', 'matplotlib==2.2.5', 'nibabel==3.0.2', 'nipype==1.8.5', 'numpy==1.18.5',
-                      'opencv_python==4.7.0.68', 'openpyxl==3.0.0', 'pandas==1.3.5', 'paramiko==2.10.1',
-                      'scikit_image==0.19.3', 'scikit_learn==0.22.2.post1', 'scipy==1.7.3',
-                      'setuptools==46.1.3', 'vtk==8.1.2', 'xlrd==2.0.1', 'XlsxWriter==3.0.3'],
+    install_requires=['antspyx==0.4.2','begins==0.9', 'Cython==3.0.6', 'dcor==0.6', 'dill==0.3.7', 'dipy==1.7.0',
+                      'fury==0.9.0', 'matplotlib==3.8.1', 'nibabel==5.1.0', 'nipype==1.8.6', 'numpy==1.26.2',
+                      'opencv_python==4.8.1.78', 'openpyxl==3.1.2', 'pandas==2.2.1', 'paramiko==3.3.1',
+                      'scikit_image==0.22.0', 'scikit_learn==1.3.2', 'scipy==1.11.3',
+                      'setuptools==58.1.0', 'vtk==9.3.0', 'xlrd==2.0.1', 'XlsxWriter==3.1.9'],
     keywords=['python', 'package', 'dipy', 'diffusion to connectomes'],
     classifiers=[
         "Development Status :: 1 - Planning",
