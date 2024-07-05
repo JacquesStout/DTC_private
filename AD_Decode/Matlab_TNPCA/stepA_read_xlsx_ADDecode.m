a4c7da42786031d4fc3e027a759471fc1a992109
Jacques Stout <jas@santorini.local>
2024-07-05 10:30:35 -0400

    adding the TN-PCA matlab main files (also requires popnet_hpca package)

---
+++ AD_Decode/Matlab_TNPCA/stepA_read_xlsx_ADDecode.m
@@ -0,0 +1,263 @@
+clear
+
+%Original location of this file is
+% /Users/jas/MATLAB/popNet_HOPCA_ADDecode_2024/ourscripts
+%was also moved to DTC_private/AD_Decode/Matlab
+
+filename = "/Users/jas/AD_Decode/CSVfiles/AD_DECODE_data3.xlsx" ;
+
+test = 0;
+act = 1;
+
+mainpath =  "/Users/jas/MATLAB/popNet_HOPCA_ADDecode_2024/";
+
+variant = ["plain"]; %Variant can be 'plain': plain connectome, 'dist': distance connectome, or 'fa': fa connectome
+%please separate folders for specific connectomes before use
+
+stripconnectivity=1; %True / False variable to determine whether connections with very low values should
+%be removed entirely (see end of file)
+
+if test
+    connectomes_folder = "/Users/jas/jacques/Figures_temp_test/Figures_MPCA_inclusive_symmetric_all/";
+    datapath = join([mainpath, "/data_test/"],"");
+else
+    if act
+        plainpath = join([mainpath, "/conn_plain_act/"],"");
+        connectomes_folder = "/Volumes/Data/Badea/Lab/mouse/mrtrix_ad_decode/connectome_act/";
+        if variant == 'dist'
+            datapath = join([mainpath, "/distances_act/"],"");
+        elseif variant == 'fa'
+            datapath = join([mainpath, "/famean_act/"],"");
+        elseif variant == 'plain'
+            datapath = plainpath;
+        end
+    else
+        connectomes_folder = "/Volumes/Data/Badea/Lab/mouse/mrtrix_ad_decode/connectome/";
+        if variant == 'dist'
+            datapath = join([mainpath, "/distances/"],"");
+        elseif variant == 'fa'
+            datapath = join([mainpath, "/famean/"],"");
+        elseif variant == 'plain'
+            datapath = join([mainpath, "/conn_plain/"],"");
+        end
+
+    end
+end
+
+variants = ["volweighted","fa"]; 
+
+size_var = size(variants);
+if size_var(1)>1
+    variants_str = '_' + strjoin(variants,'_');
+else
+    variants_str = '';
+end
+
+if ~exist(datapath, 'dir')
+   mkdir(datapath)
+end
+
+%subselect is used to only select those connectomes associiated with a subject with certain characteriscs.
+%If empty, include everyone. _genotype_4 => only APOE4 (only functional for
+%genotype at this time
+%subselect has not been used for a while and may have conflicts with the
+%connectome strip function (see end of file)
+subselect = ''; %subselect = '_genotype_4';  
+
+if test
+    getpath = join([connectomes_folder,'*',variants_str,'_connectomes.xlsx'],"");
+else
+    if variant=='fa'
+        getpath = join([connectomes_folder,'*','mean_FA_connectome.csv'],"");
+    elseif variant == 'dist'
+        getpath = join([connectomes_folder,'*',variants_str,'distances.csv'],"");
+    elseif variant == 'plain'
+        getpath = join([connectomes_folder,'*',variants_str,'conn_plain.csv'],"");
+    end
+end
+
+%Output paths, named differently if doing a subselection or on connectome
+%type
+response_array_path = join([datapath 'response_array' subselect variants_str '.mat'],"");
+response_table_path= join([datapath 'response_table' subselect variants_str '.mat'],""); 
+connectomes_path = join([datapath 'connectivity' '_ADDecode' '_mrtrix' subselect variants_str '.mat'],"");
+
+%Read the metadata for this project
+allfiles_data = readtable(filename);
+allfiles_data = allfiles_data(~(isnan(allfiles_data.MRI_Exam) | strcmp(allfiles_data.MRI_Exam, '') | allfiles_data.MRI_Exam == 0), :);
+
+
+%Create a table storing simplified metadata in Matlab Table, array
+genotypekeySet = {'APOE33', 'APOE23', 'APOE34', 'APOE44'};
+genotypevalueSet = [3 3 4 4];
+geno = containers.Map(genotypekeySet,genotypevalueSet);
+
+sexkeySet = {'M', 'F'};
+sexvalueSet = [1 2];
+sex = containers.Map(sexkeySet,sexvalueSet);
+
+response_table = table('Size',[size(allfiles_data,1) 5],'VariableNames',{'Subject', 'sex', 'MRI_Exam', 'age', 'genotype'}, 'VariableTypes',{'string','string','double', 'double','string'});
+response_array_init = zeros([size(allfiles_data,1) 4]);
+for i = 1:size(allfiles_data,1)
+    try
+        response_array_init(i,2) = geno(cell2mat(table2array(allfiles_data(i,'genotype'))));
+    catch exception
+        continue
+    end
+    response_array_init(i,1) = table2array(allfiles_data(i,'MRI_Exam'));
+    response_array_init(i,3) = table2array(allfiles_data(i,'age'));
+    response_array_init(i,4) = sex(cell2mat(table2array(allfiles_data(i,'sex'))));
+    response_table(i,1) = num2cell(table2array(allfiles_data(i,'Subject')));
+    response_table(i,2) = num2cell(table2array(allfiles_data(i,'sex')));
+    response_table(i,3) = num2cell(table2array(allfiles_data(i,'MRI_Exam')));
+    response_table(i,4) = num2cell(table2array(allfiles_data(i,'age')));
+    response_table(i,5) = num2cell(geno(cell2mat(table2array(allfiles_data(i,'genotype')))));
+end
+
+
+%Extracting all connectomes in a loop (IIT mean connectome of 84x84 is
+%assumed)
+connectivity = zeros(84,84,size(response_table,1));
+files = dir(getpath);
+subjlist = zeros(size(response_table,1),1); %List of subjects with associated connectomes
+notfoundlist = zeros(size(response_table,1),1); %List of subjects not found
+j=1;
+l=1;
+for i = 1:size(response_table,1)
+    subjname = response_table{i,3};
+    found = 0;
+    for file = files'
+        subj = strsplit(file.name,'_');
+        subj = subj{1};
+        subj = subj(3:end);
+        if subjname == str2double(subj)
+            csv = readtable(join([connectomes_folder,file.name],""));
+            if test
+                csv.Var1{84} = 'ctx-rh-insula';
+                csv = removevars(csv,{'Var1'});
+            end
+            csv = table2array(csv);
+            connectivity(:,:,j) = csv;
+            found = 1;
+            break
+        end
+    end
+    if found==1
+        %display('found '+ string(subjname))
+        subjlist(j) = subjname;
+        j = j + 1;
+    else
+        %display('did not find '+ string(subjname))
+        notfoundlist(l) = subjname;
+        l = l + 1;
+    end
+end
+
+%Creating final array, only including connectomes that were found (in the
+%same order as the connectome array itself)
+subjlist = subjlist(subjlist ~= 0);
+notfoundlist = notfoundlist(notfoundlist ~= 0);
+connectivity = connectivity(:,:,1:size(subjlist,1));
+response_array = zeros(size(subjlist,1),size(response_array_init,2));
+i=1;
+
+for k = 1:numel(response_array_init)
+    if ismember(response_array_init(k),subjlist)
+        index_subj = find(subjlist == response_array_init(k));
+        response_array(index_subj,:) = response_array_init(k,:);
+    end
+end
+
+
+%Doign the subselection based on subselect
+if size(subselect,2)>0
+    if contains(subselect,'genotype')
+        if contains(subselect,'3')
+            APOE3 = response_array(:,2)==3;
+            connectivity = connectivity(:,:,APOE3);
+            response_array = response_array(APOE3,:);
+            subjlist = subjlist(APOE3);
+        elseif contains(subselect,'4')
+            APOE4 = response_array(:,2)==4;
+            connectivity = connectivity(:,:,APOE4);
+            response_array = response_array(APOE4,:);
+            subjlist = subjlist(APOE4);
+        end
+    end
+end
+
+nan_indices = [];
+
+n_elements = size(connectivity, 3);
+nan_indices = [];
+
+% Iterate through the third dimension
+for n = 1:n_elements
+    % Check if any element in the 2D matrix at index n is NaN
+    if any(any(isnan(connectivity(:,:,n))))
+        % If NaN is found, store the index
+        nan_indices = [nan_indices, n];
+    end
+end
+subjects_failed = response_array(nan_indices,1);
+if ~isempty(subjects_failed)
+    disp('List of failed subjects with Nan values, removed')
+    disp(subjects_failed)
+end
+
+response_array = response_array(setdiff(1:n_elements, nan_indices),:);
+connectivity = connectivity(:,:,setdiff(1:n_elements, nan_indices));
+
+%Plain connectome is saved a bit differently and separately due to it being
+%used by other variants for connectome strip setup, recommended to run plain for everyone first
+connectomes_path_backup = join([plainpath 'connectivity' '_ADDecode' '_mrtrix' subselect variants_str '_backup.mat'],"");
+if variant == 'plain' && ~isfile(connectomes_path_backup)
+    connectomes_path_backup = join([datapath 'connectivity' '_ADDecode' '_mrtrix' subselect variants_str '_backup.mat'],"");
+    save(connectomes_path_backup, 'connectivity', 'subjlist');
+end
+
+%Connectome strip, removing indices where the average is lower than ten
+%connections, or where too many subjects have an index value that is less
+%than to
+if stripconnectivity && variant~='plain'
+    
+    connectomes_plain_backup = load(connectomes_path_backup);
+    connectivity_plain = connectomes_plain_backup.('connectivity');
+    subjlist_plain = connectomes_plain_backup.('subjlist');
+    if ~all(subjlist==subjlist_plain)
+        throw('subjlist is different, redo plain connectome')
+    end
+    
+    % Assuming your 3D matrix is named 'data'
+    % N is the size of the third dimension
+
+    % Step 1: Calculate the average for each ixj slice over the third dimension
+    average_values = mean(connectivity_plain, 3);
+
+    % Step 2: Identify indices where the average value is lower than 10
+    low_average_indices = average_values < 10;
+
+    % Step 3: Count instances where ixj has value 0 in the third dimension
+    zero_count = sum(connectivity_plain == 0, 3);
+
+    % Step 4: Identify indices where there are already 10 or more instances of ixj being 0
+    too_many_zeros_indices = zero_count >= 10;
+
+    % Step 5: Identify indices where either condition is true
+    indices_to_remove = low_average_indices | too_many_zeros_indices;
+
+    % Step 6: Set values to 0 for the identified indices
+    num_subjs = size(connectivity,3);
+    connectivity_new = zeros(size(connectivity));
+    connectivity_plain_new = zeros(size(connectivity));
+    for subj = 1:num_subjs
+        connectivity_new(:, :, subj) = connectivity(:, :, subj) .* ~indices_to_remove;
+        connectivity_plain_new(:, :, subj) = connectivity_plain(:, :, subj) .* ~indices_to_remove;
+    end
+    connectivity = connectivity_new;
+end
+
+save(response_array_path, 'response_array');
+%save(response_table_path, 'response_table');
+save(connectomes_path, 'connectivity', 'subjlist');
+
---
+++ AD_Decode/Matlab_TNPCA/stepBC_TNPCA_calculatevars_ADDecode.m
@@ -0,0 +1,319 @@
+%-------------------------------Script Information-------------------------
+% This script is used for extrcting Principle Components by Tensor PCA and
+% also help you decide how many PCs to use
+% Author: Jacques Stout & Wenlin Wu
+% Last change:2024-07-4
+% Change: cleanup and commenting update
+
+%-------------------------------Output Information-------------------------
+%U: subject mode, store PC score for the data;
+%V: network modem store network basis;
+%percent_store: store variation information that top PCs can reflect
+
+%-------------------------------Setup Parameter----------------------------
+clear
+
+addpath('/Users/jas/MATLAB/tensor_toolbox')
+
+mainpath =  "/Users/jas/MATLAB/popNet_HOPCA_ADDecode_2024/";
+
+act = 1;
+
+%if act
+%    datapath = join([mainpath, "/distances_act/"],"");
+%    outpath =join([mainpath '/results_distances_act/'],"");
+%else
+%    datapath = join([mainpath, "/distances/"],"");
+%end
+
+%variants = ['*'];
+
+%variant = ['plain'];
+variant = 'fa' ;
+
+%variants = ["volweighted","fa"];
+size_var = size(variant);
+if size_var(1)>1
+    variants_str = '_' + strjoin(variant,'_');
+else
+    %variants_str = '';
+    if strcmp(variant,'plain')
+        variant_str = '';
+    else
+        variant_str = join(['_' variant],"");
+    end
+end
+
+
+%Should be kept consistent with stepA code, 
+%clarifies the connectome type, 'plain', 'fa', 'distances', etc
+
+if act
+    connectomes_folder = "/Volumes/Data/Badea/Lab/mouse/mrtrix_ad_decode/connectome_act/";
+    %if isempty(variant)
+    %    datapath = join([mainpath, "/distances_act/"],"");
+    %    outpath =join([mainpath '/results_distances_act/'],"");
+    if strcmp(variant,'plain')
+        datapath = join([mainpath, "/conn_plain_act/"],"");
+        outpath =join([mainpath '/results_conn_plain_act/'],"");
+    elseif strcmp(variant,'fa')
+        datapath = join([mainpath, "/famean_act/"],"");
+        outpath =join([mainpath '/results_famean_act/'],"");
+    end
+%else
+%    connectomes_folder = "/Volumes/Data/Badea/Lab/mouse/mrtrix_ad_decode/connectome/";
+%    datapath = join([mainpath, "/distances/"],"");
+%    if isempty(variant)
+%        datapath = join([mainpath, "/distances/"],"");
+%        outpath =join([mainpath '/results_distances/'],"");%
+%
+%    elseif variant == 'fa'
+%        datapath = join([mainpath, "/famean/"],"");
+%        outpath =join([mainpath '/results_famean/'],"");
+%    end
+end
+
+
+%outpath = '/Users/alex/Matlab/popNet_HOPCA_ADDecode_2024/results/';
+data_source = 2; %1-endstreams, 2-inclusive
+trial = '_1'; %today 1st try **important otherwise will overwrite other trials today
+%type = 1; %Please pick [1-'All', 2-'noYoung', 3-'onlyYoung']
+orth = 1; %Please pick [1-'V_orth', '2-U,V,W_orth']note:we usually orth = 1
+gen_type = 1;%Please pick [1-'All', 2-'gen3&4', 3-'only gen0']
+num_con = 152;
+k = 30; % # of factors to extract
+date = datestr(now,30);
+
+%----------------Main Part of TensorPCA&Variation identify-----------------
+
+analysis_type = 'age'; %genotype, sex, age
+
+if strcmp(analysis_type,'genotype')
+    name = 'genotype_comparison';
+elseif strcmp(analysis_type,'sex')
+    name = 'sex_comparison';
+elseif strcmp(analysis_type,'age')
+    name = 'age_comparison';
+end
+
+% _genotype_4 _genotype_3 
+subselect = '';
+
+%load connectivity data
+if data_source == 1
+    connectomes_path = join([datapath 'connectivity_all' '_ADDecode' '_inclusive_Dipy.mat'],"");
+    %connectomes_path = join([mypath 'connectivity_all' '_ADDecode' '_Dipy' subselect variants_str '.mat'],"");
+    result_path = [ outpath '/myresults_' name '_inclusive.txt'];
+    load(connectomes_path);
+elseif data_source == 2
+    %connectomes_path = join([datapath 'connectivity' '_ADDecode' '_mrtrix' subselect variant_str '.mat'],"");
+    connectomes_path = join([datapath 'connectivity' '_ADDecode' '_mrtrix' subselect '.mat'],"");
+    result_path = join([outpath '/myresults_' name subselect variant_str '.txt'],"");
+    %response_array_path = join([datapath 'response_array' subselect variant_str '.mat'],"");
+    response_array_path = join([datapath 'response_array' subselect '.mat'],"");
+    load(connectomes_path);
+end
+%elseif data_source == 2
+%    outpath = [outpath 'Dipy_inclusive/'];
+%    connectome_path = join([mypath 'connectivity_all' '_ADDecode' '_inclusive_Dipy.mat'],"");
+%    load(connectome_path);
+%elseif data_source == 3
+%    outpath = [outpath 'Dipy_inclusive/'];
+%    connectome_path = join([mypath 'connectivity_all' '_AD_Decode' 'inclusive_Dipy.mat']);
+%    load(connectome_path);   
+%    connectivity = connectivity_diff;
+%end
+
+if ~exist(outpath, 'dir')
+   mkdir(outpath)
+end
+
+if data_source == 1 || data_source == 2
+    
+    load(response_array_path);
+    response = response_array;
+    test = ismember(response,[3 4]);
+    
+    APOE3 = response(response(:,2)==3, 1);
+    APOE4 = response(response(:,2)==4, 1);
+
+    Male = response(response(:,4)==1, 1);
+    Female = response(response(:,4)==2, 1);
+    
+    %connectivity = connectivity(:,:,idx_used(:,1));
+    X = tensor(connectivity);
+    
+    %idx_AMD = (idx_used ==  3 | idx_used == 4);
+    %idx = idx_AMD(:,2); 
+end
+
+if orth == 1
+    [V,D,U,W,Xhat,obj] = hopca_popNet(X,k,foptions);
+    orth_type = 'V';
+else
+    [V,D,U,W,Xhat,obj] = hopca_popNet_new(X,k,foptions);
+    orth_type = 'UVW';
+end
+
+%Calculate variation percentage
+% U subject mode
+%V network mode
+PCs.U = U;
+PCs.V = V;
+percent_store = []; %help select # of PCs
+for i = 1:k
+    [percent] = var_explained(X,i,PCs);
+    percent_store = cat(2, percent_store, [i;percent]);
+    disp(['top ' num2str(i) '/' num2str(k) ' PCs explain ' num2str(percent) '% variance of X'])
+end
+%datapath = join([outpath date(3:8) trial '_TNPCA_' orth_type '_' num2str(type) '_K' num2str(k) '_' name '_Outputs.mat'],"");
+datapath = join([outpath date(3:8) trial '_TNPCA_' orth_type '_K' num2str(k) '_' name '_Outputs.mat'],"");
+save(datapath,'U','V','D','percent_store','obj','connectivity');
+%clearvars -except mypath datapath idx_used name idx outpath response_array result_path analysis_type;
+load(datapath);
+
+%load([mypath 'response_all.mat']);
+%control_idx = control_idx_init_paired;
+%AMD_idx = AMD_idx_init_paired;
+%idx_control = (idx_init_paired ==  1 | idx_init_paired ==  3 | idx_init_paired ==  4);
+
+%idx_AMD = (idx_used ==  2 | idx_used ==  5 | idx_used == 6);
+%idx = idx_AMD(:,2);
+
+if strcmp(analysis_type,'genotype')
+    idx = response_array(:,2);
+    idx_4 = idx==4;
+    idx = idx_4;
+elseif strcmp(analysis_type, 'sex')
+    idx = response_array(:,4);
+    idx_M = idx==1;
+    idx = idx_M;    
+elseif strcmp(analysis_type,'age')
+    idx = response_array(:,3);
+    idx_old = idx>50;
+    idx = idx_old;  
+end
+
+data = cat(2,U(:,1:10),idx);
+[w,t,fp]=fisher_training(data(:,1:end-1),data(:,end));
+w = w/norm(w);
+u0 = mean(data(data(:,end)==0,1:end-1));
+u1 = mean(data(data(:,end)==1,1:end-1));
+s = norm(u1-u0);
+
+Net_change = zeros(84,84);
+for i = 1:size(data(:,1:end-1),2)
+    base_net = D(i)*w(i)*(V(:,i)*V(:,i)');
+    Net_change = Net_change+base_net;
+    %disp(i)
+end
+Net_change = s*Net_change;
+
+%remove diagnal
+Net_change = Net_change-diag(diag(Net_change));
+
+k = 200;
+maxcon = maxk(Net_change(:),k);
+mincon = mink(Net_change(:),k);
+mostcon = cat(1,maxcon,mincon);
+mostcon = sort(mostcon,'descend','ComparisonMethod','abs');
+
+%find the ROI index
+ridx = [];
+cidx = [];
+for j = 1:2:k
+    disp(mostcon(j))
+    [r,c] = find(Net_change==mostcon(j));
+    ridx = cat(1,ridx,r);
+    cidx = cat(1,cidx,c);
+end
+mostcon_idx = [ridx cidx];
+mostcon_idx = mostcon_idx(1:2:k,:);
+
+%Autonomy_data = readtable('C:\Users\Jacques Stout\Documents\MATLAB\popNet_HOPCA_whiston_2\data\anatomyInfo_whiston_new.csv');
+Autonomy_data = readtable('/Users/alex/code/Matlab/popNet_HOPCA_whiston/data/anatomyInfo_whiston_new.csv');
+%disp(' ')
+%disp(' ')
+%disp(['the top' num2str(k/2) ' connected subnetwork contribute to distinguish gen3 and gen4 '])
+%disp(['the top' num2str(k/2) ' connected subnetwork contribute to distinguish old and young '])
+disp(['the top' num2str(k/2) ' connected subnetwork contribute to distinguish control and AMD '])
+
+%disp(' ')
+
+dot_txt_position = strfind(result_path, '.txt');
+result_path_char = char(result_path);
+result_path_nodes = [result_path_char(1:dot_txt_position(end)-1), '_nodes.txt'];
+
+fileID = fopen(result_path,'w');
+%fileID = fopen(['C:\Users\Jacques Stout\Documents\MATLAB\popNet_HOPCA_whiston_2\results/myresults_' name '.txt'],'w');
+for h = 1:size(mostcon_idx,1)'
+    %h rank of pair
+    %con1 Region 1 name
+    %d1 - left or right
+    %con2
+    %d2
+    %i1, i2 -index of regions
+    i1 = mostcon_idx(h,1);
+    i2 = mostcon_idx(h,2);
+    con1 = char(Autonomy_data(i1,2).Variables);
+    d1 = char(Autonomy_data(i1,3).Variables);
+    con2 = char(Autonomy_data(i2,2).Variables);
+    d2 = char(Autonomy_data(i2,3).Variables);
+    group1_mean = mean(connectivity(i1,i2,idx));
+    group2_mean = mean(connectivity(i1,i2,~idx));
+    %txt = ([num2str(h) ' ' con1 '_' d1 '---' con2 '_' d2 ' ' num2str(i1) ' ' num2str(i2) ' with weight of ' num2str((maxcon(h*2)/(0.01*s))) ' and avg difference of ' diff_val '\n']);
+    txt = ([num2str(h) ' ' con1 '_' d1 '---' con2 '_' d2 ' ' num2str(i1) ' ' num2str(i2) ' with weight of ' num2str((maxcon(h*2)/(0.01*s))) '\n']);
+    fprintf(fileID,txt);
+    display(txt);
+    %disp(' ')
+end
+fclose(fileID);
+
+for h = 1:size(mostcon_idx,1)'
+    i1 = mostcon_idx(h,1);
+    i2 = mostcon_idx(h,2);
+    APOE4_mean = mean(connectivity(i1,i2,idx));
+    APOE3_mean = mean(connectivity(i1,i2,~idx));
+    diff_mean = APOE4_mean - APOE3_mean;
+end
+    
+
+maxcon_evened = maxcon(2:2:end);    
+mostcon_idx_2 = [mostcon_idx, maxcon_evened];
+
+unique_integers = unique(vertcat(unique(mostcon_idx_2(:, 1), 'rows'), unique(mostcon_idx_2(:, 2), 'rows')));
+num_integers = size(unique_integers);
+
+integer_weights = zeros(num_integers(1),2);
+i=1;
+
+for integer=unique_integers'
+    row_index = unique(vertcat(find(mostcon_idx_2(:,1) == integer),find(mostcon_idx_2(:,2) == integer)));
+    sumweights = sum(mostcon_idx_2(row_index,3));
+    integer_weights(i,1)=integer;
+    integer_weights(i,2)=sumweights;
+    i= i + 1;
+    %con = char(Autonomy_data(integer,2).Variables);
+    %d = char(Autonomy_data(integer,3).Variables);
+    
+    %txt = ([con '_' d ' has total weight of ' num2str((sumweights/(0.01*s))) '\n']);
+    %display(txt);
+end
+
+integer_weights = sortrows(integer_weights, -2);
+
+fileID = fopen(result_path_nodes,'w');
+
+for i=1:size(integer_weights,1)
+    integer = integer_weights(i,1);
+    sumweights = integer_weights(i,2);
+    con = char(Autonomy_data(integer,2).Variables);
+    d = char(Autonomy_data(integer,3).Variables);
+    
+    txt = ([con '_' d ' has total weight of ' num2str((sumweights/(0.01*s))) '\n']);
+    fprintf(fileID,txt);
+
+    display(txt);
+end
+fclose(fileID);
+
